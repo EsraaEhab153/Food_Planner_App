87bccb59b9243a6ece9dd19efd257939d9fcc554
esraa_ehab <esraaehab530@gmail.com>
2026-02-04 21:22:54 +0200

    finished forget password ui

---
+++ app/src/main/java/com/example/foodplannerapp/ui/auth/forgetPassword/ForgetPasswordFragment.java
@@ -0,0 +1,29 @@
+package com.example.foodplannerapp.ui.auth.forgetPassword;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.foodplannerapp.R;
+
+public class ForgetPasswordFragment extends Fragment {
+
+    public ForgetPasswordFragment() {
+        // Required empty public constructor
+    }
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_forget_password, container, false);
+    }
+}
