ce194eb6bcae90edd347aca55e7875c9c9fd1088
esraa_ehab <esraaehab530@gmail.com>
2026-02-05 18:56:06 +0200

    finished navGraph and navigation between auth screens

--- app/src/main/java/com/example/foodplannerapp/ui/auth/login/LoginFragment.java
+++ app/src/main/java/com/example/foodplannerapp/ui/auth/login/LoginFragment.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
 
 import android.text.method.HideReturnsTransformationMethod;
 import android.text.method.PasswordTransformationMethod;
@@ -12,6 +14,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.foodplannerapp.MainActivity;
@@ -23,6 +26,7 @@ private LoginPresenter presenter;
 EditText emailEdt , passwordEdt;
 TextInputLayout passwordLayout;
 private boolean isPasswordVisible = false;
+TextView tvRegisterNow,tvForgetPassword;
 
     public LoginFragment() {
         // Required empty public constructor
@@ -43,7 +47,9 @@ private boolean isPasswordVisible = false;
     passwordEdt = view.findViewById(R.id.login_password);
     Button loginBtn = view.findViewById(R.id.btn_signin);
     passwordLayout = view.findViewById(R.id.password_input_layout);
-
+    tvRegisterNow = view.findViewById(R.id.tv_register_now);
+    tvForgetPassword = view.findViewById(R.id.tv_forget_password);
+    NavController navController =NavHostFragment.findNavController(this);
         passwordLayout.setEndIconOnClickListener(v -> {
 
             if (isPasswordVisible) {
@@ -66,6 +72,13 @@ private boolean isPasswordVisible = false;
 
     });
 
+    tvRegisterNow.setOnClickListener(view2 ->
+        navController.navigate(R.id.action_login_to_register)
+    );
+
+    tvForgetPassword.setOnClickListener(view3 ->
+            navController.navigate(R.id.action_login_to_forgot)
+            );
         return view;
     }
 
